500dc8d3e7717da9d799f3e0db2e0743ba22e374
Shubh <shughit@gmail.com>
2022-01-04 22:26:14 +0530

    intial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e2df5ce5c0dd51bfa40b886b2aa9440c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1ca55d0a-6a09-4d1b-8a5f-cb4f83097c10'
+};
